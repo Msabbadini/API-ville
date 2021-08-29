2ae84880637a73f39761a7ccd8daced80e6e640b
Sabbadini <maximefitz.sabbadini@gmail.com>
2021-08-29 10:49:12 +0200

    v0.5

--- views/index.php
+++ views/index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once "views/common/header.php" ?> 
-<body class='is-preload'>
+<body>
     <H1>Listes des commandes de l'api</H1>
 
-    <p>Dans la barre de recherche vous avez plusieurs possibilités pour utiliser l'API, voici une liste des commandes</p>
+    <p>Dans la barre de recherche après le "/",  vous avez plusieurs possibilités pour utiliser l'API, voici une liste des commandes</p>
     <ul>
         <li>villes/{code du département}</li>
-        <p>Exemple villes/83 => vous obtiendrez la liste des villes du département 83</p>
-        <li>/villes/{code du département}/{code du canton}</li>
+        <li>villes/{code du département}/{code du canton}</li>
+        <li>ville/{code postal}</li>
+        <li>population/{code postal}</li>
+        <li>superficie/{code postal}</li>
+        <li>densite/{code postal}</li>
+        <li>ville/{code postal}/update</li>
     </ul>
 </body>
 </html>
